094976f841d5369481aea6149f96bc7585707b73
nmehra98 <nmehra2398@gmail.com>
2018-03-23 14:40:04 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello world")
+print('What\'s up?')
+print('Let\'s get started')
+
+##asking name##
+
+spy_name = input("What is your spy name?")
+print(spy_name)
+print("Welcome"+""+spy_name+"in Acadview")
+
+spy_salutation = input("What should we call you (Mr. or Ms.)?")
+print(spy_salutation)
+print(spy_salutation+""+spy_name)
+
+spy_name = spy_salutation+""+spy_name
+print(spy_name)
