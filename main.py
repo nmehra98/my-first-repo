5e6bc358a6fc261dcc47a0d5b6de279cecdc293f
nmehra98 <nmehra2398@gmail.com>
2018-03-25 20:43:49 +0530

    my first commit

--- main.py
+++ main.py
@@ -28,15 +28,42 @@ else:
     print("Not a valid name....Try again")
 
 #####new user#####
-spy_age = 0
-spy_rating = 0.0
-spy_is_online = False
-#####asking age######
+def spydetails(spy_name,spy_age,spy_rating):
+    menu = "What you want to do? \n 1.Add status \n 2. Quit"
+    menu1 = int(input(menu))
+    if (menu1==1):
+        print("Status")
+    else:
+        print("Quit !!!")
+
+spy_name = "Michael"
+spy_salutation = "Mr."
+spy_age = 20
+spy_rating = 3.5
+
+ques = "Do you want to continue as " + spy_salutation + " " + spy_name + "Y/N ?"
+var = input(ques)
+
+if var == "Y":
+    spydetails(spy_name, spy_age, spy_rating)
+else:
+    spy_name = input("Welcome!!! you must tell your spy name first.")
+    if len(spy_name) > 0:
+        spy_name = input("What is your name?")
+        spy_salutation = input("What should we call you (Mister or Misses)?")
+        print("Welcome" + " " + spy_salutation + " " + spy_name + "to Acadview")
+        spy_name = spy_salutation + " " + spy_name
+        print(spy_name)
+        print("Alright" + " " + spy_name + " " + " we would like to know more about you...")
+    else:
+        print("Not a valid name....Try again")
+
 spy_age = int(input("Enter your age : "))
-if spy_age > 12 and spy_age < 50 :
+if spy_age > 12 and spy_age < 50:
     print("Valid spy.")
 else:
     print("Not a valid spy")
+
 spy_rating = float(input("Enter the spy rating : "))
 if spy_rating > 4.5:
     print("Great ace...")
@@ -49,3 +76,6 @@ else:
 
 spy_is_online = True
 print("Authentication complete.... Welcome " + str(spy_name) + " age: " + str(spy_age) + " and rating of: " + str(spy_rating) + " proud to have you onboard")
+
+
+
